3f7fba599a75f8d9deb40179bfca71af2418129e
alvrs <alvarius@lattice.xyz>
2022-10-22 20:09:55 +0100

    feat: add link to sources to index.json

--- scripts/list.js
+++ scripts/list.js
@@ -2,6 +2,11 @@ import glob from "glob";
 import fs from "fs";
 
 glob("./plugins/*", (_, plugins) => {
+  const index = {
+    plugins: plugins.map((p) => "/" + p.replace("./plugins/", "") + ".js"),
+    source: "https://github.com/latticexyz/opcraft-plugins",
+  };
+
   fs.mkdirSync("./www");
-  fs.writeFileSync("./www/index.json", JSON.stringify(plugins.map((p) => "/" + p.replace("./plugins/", "") + ".js")));
+  fs.writeFileSync("./www/index.json", JSON.stringify(index));
 });
